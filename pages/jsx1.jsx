b7941e67a2497e66364ace3e24b7f1c8a209b877
Pedro Valente <valentedev09@gmail.com>
2022-03-10 22:11:14 -0300

    aula 7

---
+++ pages/jsx1.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function primeiro() {
+    return (
+        <div className={styles.container}>
+            <h1>Primeiro</h1>
+            <h2>Segundo</h2>
+            <h3>Terceiro</h3>
+        </div>
+    )
+}
